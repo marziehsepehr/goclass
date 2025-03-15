a82d7fd4bce50a216b9cf59d29af2eb4ca8677f2
marziehsepehr <marziehsepehr@gmail.com>
2025-03-16 02:05:26 +0330

    Add if

--- main.go
+++ main.go
@@ -1,31 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
+// func main() {
+// 	//
+// 	var i = 0
+// 	fmt.Println(i)
+// 	//
+// 	sum := 0
 
+// 	for i := 0; i < 10; i += 2 {
+// 		sum += i
+// 	}
+
+// 	fmt.Println(sum)
+// 	//
+
+// 	j := 0
+
+// 	for j < 100 {
+// 		j = j + 1
+// 		fmt.Println(j)
+
+// 	}
+
+// }
+
+func sqrt(x float64) string {
+	if x < 0 {
+		return sqrt(-x) + "i"
+	}
+
+	return fmt.Sprint(math.Sqrt(x))
+}
 
 func main() {
-	//
-	var i = 0
-	fmt.Println(i)
-	//
-	sum := 0
 
-	for i := 0; i < 10; i += 2 {
-		sum += i
-	}
-
-	fmt.Println(sum)
-	//
-
-	j := 0
-
-	for j < 100 {
-		j = j + 1
-		fmt.Println(j)
-
-	}
-
-
-
+	fmt.Println(sqrt(4), sqrt(-6))
 }
