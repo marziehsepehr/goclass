a82c233971be9ca4d98a34042525b2602cd0ebbb
marziehsepehr <marziehsepehr@gmail.com>
2025-03-20 04:07:14 +0330

    Modify println and Exit with log.Fatalf

--- session2/main.go
+++ session2/main.go
@@ -16,9 +16,13 @@ func main() {
 		name := args[1]
 		fmt.Println(name)
 	} else {
-		fmt.Println("error: At least one argument is needed")
-		os.Exit(1)
-		
+		/*
+			fmt.Println("error: At least one argument is needed")
+			os.Exit(1)
+		*/
+		// Instead of print in colcole and exit with two line ==>logs.Fatalf
+		log.Fatalf("There is no input, Len of args is : %d", len(args))
+
 	}
 
 }
