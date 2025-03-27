47a9bc540fe09d8f2c220678ed3bfa7008c4fd48
marziehsepehr <marziehsepehr@gmail.com>
2025-03-27 18:21:58 +0330

    Add pars args with flag

--- session2/main.go
+++ session2/main.go
@@ -1,38 +1,58 @@
-//parse args without using flag package
+// //parse args without using flag package
+
+// package main
+
+// import (
+// 	"fmt"
+// 	"log"
+// 	"os"
+// 	"strings"
+// )
+
+// //	comman-line interface
+// //
+// // gui
+// func main() {
+
+// 	args := os.Args
+// 	if len(args) > 1 {
+// 		name := args[1]
+// 		fmt.Println(name)
+// 	} else {
+// 		/*
+// 			fmt.Println("error: At least one argument is needed")
+// 			os.Exit(1)
+// 		*/
+// 		// Instead of print in colcole and exit with two line ==>logs.Fatalf
+// 		log.Fatalf("There is   no input, Len of args is : %d", len(args))
+
+// 	}
+
+// 	name := args[1]
+// 	fmt.Println("Hello", name)
+// 	name2 := args[2]
+// 	fmt.Println("Hello", name2)
+// 	name = strings.Replace(name, "--first_name=", "", 1)
+// 	fmt.Println("Hello", name)
+
+// }
+
+// parse args with flag package
 
 package main
 
 import (
+	"flag"
 	"fmt"
-	"log"
-	"os"
-	"strings"
 )
 
-//	comman-line interface
-//
-// gui
 func main() {
+	firstName := flag.String("first_name", "default", "first name")
+	lastName := flag.String("last_name", "default", "last name")
 
-	args := os.Args
-	if len(args) > 1 {
-		name := args[1]
-		fmt.Println(name)
-	} else {
-		/*
-			fmt.Println("error: At least one argument is needed")
-			os.Exit(1)
-		*/
-		// Instead of print in colcole and exit with two line ==>logs.Fatalf
-		log.Fatalf("There is   no input, Len of args is : %d", len(args))
+	flag.Parse()
+	fmt.Println("name", *firstName, *lastName, flag.Args())
 
-	}
-
-	name := args[1]
-	fmt.Println("Hello", name)
-	name2 := args[2]
-	fmt.Println("Hello", name2)
-	name = strings.Replace(name, "--first_name=", "", 1)
-	fmt.Println("Hello", name)
+	fmt.Println("len of remaining args", len(flag.Args()))
 
 }
