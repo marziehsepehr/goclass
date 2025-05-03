12e1266bdc0f7032eba44dc0dfc7f6a0124cde62
marziehsepehr <marziehsepehr@gmail.com>
2025-05-03 09:04:12 +0330

    Added map

---
+++ session3/main.go
@@ -0,0 +1,43 @@
+package main
+
+import "fmt"
+
+type Point struct {
+	lat, long float64
+}
+
+// global map decleration with key as string and value a Point struct
+var points map[string]Point
+
+func main() {
+
+	if points == nil {
+
+		fmt.Println("not init yet!")
+	}
+	// initialize the map
+	points = make(map[string]Point)
+	if points == nil {
+
+		fmt.Println("not init yet!")
+	} else {
+		fmt.Println("initialized!")
+	}
+	points["Home"] = Point{34.555, 43.222}
+	points["Home2"] = Point{34.555, 43.222}
+	points["Home3"] = Point{34.555, 43.222}
+	fmt.Println(points["Home"])
+
+	fmt.Println("All points are:", points)
+	var k = "Home3"
+	value, ok := points[k]
+	if ok {
+		fmt.Printf("key: %s exist in points and the value is lat: %0.3f long:%0.3f\n", k, value.lat, value.long)
+		delete(points, k)
+		fmt.Printf("after deleteing %s\n", k)
+		fmt.Println("All points are:", points)
+	} else {
+		fmt.Printf("key: %s not exist in points", k)
+	}
+
+}
